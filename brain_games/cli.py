c5bb88c10339a52ecb8c5c23d9c116e0406f8231
nikolaev <r.ferius@gmail.com>
2023-03-19 23:00:33 +0500

    Added cli.py that handles user input. Add prompt for user input.

---
+++ brain_games/cli.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""Welcome user script."""
+import prompt
+
+
+def welcome_user():
+    """Get name and greeting to user."""
+
+    name = prompt.string('May I have your name? ')
+    print('Hello, {0}!'.format(name))
+
+
+if __name__ == '__main__':
+    welcome_user()
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,4 +1,14 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+"""Main Brain games module."""
+
+
+from brain_games.cli import welcome_user
+
 
 def main():
-    print("Welcome to the Brain Games!")
+    print('Welcome to the Brain Games!')
+    welcome_user()
+
+
+if __name__ == '__main__':
+    main()
