d4ff6a218e9cea5cd3ccaf050239eb23a3340ec6
nikolaev <r.ferius@gmail.com>
2023-04-02 00:04:53 +0500

    Using a ternary operator instead of branching

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -6,10 +6,7 @@ DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 def make_question():
     task = randint(1, 100)
     flag = is_even(task)
-    if flag is True:
-        correct_answer = 'yes'
-    else:
-        correct_answer = 'no'
+    correct_answer = 'yes' if flag else 'no'
     return correct_answer, task
 
 
