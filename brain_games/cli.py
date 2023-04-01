1b249f9284525452914b9710b4dd3f6b7148da11
nikolaev <r.ferius@gmail.com>
2023-04-02 00:01:12 +0500

    cli.py - delete unused func

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -8,24 +8,5 @@ def greet():
     return name
 
 
-def core(game):
-    name = greet()
-    print(game.DESCRIPTION)
-    counter = 0
-
-    while counter < 3:
-        correct_answer, task = game.make_question()
-        print(f'Question: {task}')
-        user_answer = prompt.string('Your answer: ')
-
-        if user_answer == correct_answer:
-            print('Correct!')
-            counter += 1
-
-        else:
-            print(f'\'{user_answer}\' is wrong answer ;(. '
-                  f'Correct answer was \'{correct_answer}\'.'
-                  f"\nLet's try again, {name}!")
-            return
-
-    print(f'Congratulations, {name}!')
+if __name__ == '__main__':
+    greet()
