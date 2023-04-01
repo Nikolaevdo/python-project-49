2775540e25835e0fb77a5788de68b1ab01f2f606
nikolaev <r.ferius@gmail.com>
2023-04-02 00:18:21 +0500

    Renamed func from core to run_game

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.engine import core
+from brain_games.engine import run_game
 from brain_games.games import calc
 
 
 def main():
-    core(calc)
+    run_game(calc)
 
 
 if __name__ == '__main__':
--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,10 +1,10 @@
 # !/usr/bin/env python3
-from brain_games.engine import core
+from brain_games.engine import run_game
 from brain_games.games import prime
 
 
 def main():
-    core(prime)
+    run_game(prime)
 
 
 if __name__ == '__main__':
